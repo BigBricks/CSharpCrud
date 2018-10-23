d4ca14efa42aaf0c92f241dbb349ec24017f0503
BigBricks <BSA6.23.94@gmail.com>
2018-10-23 14:47:05 -0400

    models and todo item model

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public bool Complete { get; set; }
+    }
+}
