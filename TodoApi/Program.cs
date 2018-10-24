d9d9129379d3f98a42d71f2a39aa88fb50033e3d
BigBricks <BSA6.23.94@gmail.com>
2018-10-24 14:05:53 -0400

    heroku caused problems

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -14,16 +14,7 @@ namespace TodoApi
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseConfiguration(config)
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
